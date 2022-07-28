9dc301358f021eae70c3302037d4235018cb76e8
monlica <wattana.mcs@gmail.com>
2022-07-28 18:55:53 +0700

    7/28/2022

---
+++ app/src/main/java/com/example/ass5intent2022/SecondActivity.kt
@@ -0,0 +1,32 @@
+package com.example.ass5intent2022
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.ass5intent2022.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var bindingSecond : ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        bindingSecond = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(bindingSecond.root)
+
+        var data = intent.extras
+        var newEmployee = data?.getParcelable<Employee>("empData")
+
+        bindingSecond.nameText.text = "Employee Name : ${newEmployee?.name}"
+        bindingSecond.genderText.text  = "Gender : ${newEmployee?.gender}"
+        bindingSecond.emailText.text  = "E-mail : ${newEmployee?.email}"
+        bindingSecond.salaryText.text  = "Salary : ${newEmployee?.salary} Baht"
+
+    }
+    fun onClickBack(v: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
