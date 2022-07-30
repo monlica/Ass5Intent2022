0b7261f9e07182f8ba3ac8ca35ad627f55846a4b
monlica <wattana.mcs@gmail.com>
2022-07-30 19:01:34 +0700

    ok

---
+++ app/src/main/java/com/example/ass5intent2022/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.ass5intent2022
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.example.ass5intent2022.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun showResult(view : View){
+
+        var  radioGroup: RadioGroup =  findViewById(R.id.radioGroup)
+        var  selectedId : Int = radioGroup.checkedRadioButtonId
+        var  radioButton : RadioButton = findViewById(selectedId)
+
+
+        val intent = Intent(this, SecondActivity::class.java)
+        intent.putExtra("empData", Employee(binding.editName.text.toString(),radioButton.text.toString(),
+            binding.editEmail.text.toString(), binding.editSalary.text.toString().toInt()))
+        startActivity(intent)
+
+    }
+
+
+    fun resetData(v: View){
+        binding.editName.text?.clear()
+        //edit_password.text.clear()
+        binding.editEmail.text?.clear()
+        binding.editSalary.text?.clear()
+        var  radioGroup: RadioGroup =  findViewById(R.id.radioGroup)
+        radioGroup.clearCheck()
+
+    }
+}
